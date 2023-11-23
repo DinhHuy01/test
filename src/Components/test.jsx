2866d4534fc0af618fb84a09d773a44041bebc93
dinhhuy <150763766+DinhHuy01@users.noreply.github.com>
2023-11-23 15:21:21 +0700

    Update test.jsx

--- src/Components/test.jsx
+++ src/Components/test.jsx
@@ -2,22 +2,9 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 const Test = () => {
-    const [info, setInfo] = useState([]);
+    
 
-    useEffect(() => {
-        axios
-            .post('https://reqres.in/api/users', {
-                name: 'Huy',
-                job: 'IT',
-            })
-            .then((res) => {
-                // setInfo(res.data.data);
-                console.log(res);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }, []);
+
     return (
         <>
             {/* {info.map((index) => (
